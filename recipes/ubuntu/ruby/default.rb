50aea414841be35eacf5fa167175f9084131f60b
Takashi Kokubun <takashikkbn@gmail.com>
2024-08-22 15:12:35 -0600

    "better test for cargo_install - add chruby plugin

--- recipes/ubuntu/ruby/default.rb
+++ recipes/ubuntu/ruby/default.rb
@@ -1,3 +1,5 @@
+include_recipe 'nm_chruby/system'
+
 dotfile ".config/irb/irbc"
 dotfile ".config/gem/gemrc"
 # Ruby build dependencies
@@ -8,3 +10,7 @@ package "libssl-dev"
 # Ruby source build dependencies
 package "autoconf"
 package "bison"
+
+%w(standardrb solargraph ruby-lsp).each do |gem|
+  gem_package gem
+end
--- recipes/ubuntu/rust.rb
+++ recipes/ubuntu/rust.rb
@@ -1,30 +1,49 @@
 include_recipe "rust::user"
-
+cargo_home = node[:rust][:cargo_home]
+cargo_cmd = "#{cargo_home}/bin/cargo"
 # define cargo_install command
-define :cargo_install, version: nil, locked: true, path: nil, git: nil, features: nil do
+define :cargo_install, version: nil, locked: true, path: nil, git: nil, 
+  features: nil, binname: nil  do
   cargo_name = params[:name]
-  cmd = "cargo install #{cargo_name}"
+  binname = params[:binname] || params[:name]
+  cmd = "#{cargo_cmd} install #{cargo_name}"
   cmd << " --version #{params[:version]}" if params[:version]
   cmd << " --path #{params[:path]}" if params[:path]
   cmd << " --git #{params[:git]}" if params[:git]
   cmd << " --features #{params[:features]}" if params[:features]
   cmd << " --locked" if params[:locked]
-  execute cmd
+  execute "installing_cmd" do
+    command cmd
+    not_if "test -e #{cargo_home}/bin/#{binname}"
+  end
 end
 
 cargo_install "sccache"
+
 # add RUSTC_WRAPPER to .bashrc
 execute "add RUSTC_WRAPPER to .bashrc" do
   command "echo 'export RUSTC_WRAPPER=#{ENV["HOME"]}/.cargo/bin/sccache' >> #{ENV["HOME"]}/.bashrc"
   not_if "grep 'export RUSTC_WRAPPER' #{ENV["HOME"]}/.bashrc"
 end
 
-cargo_install "cargo-update", locked: false
-cargo_install "broot", features: "clipboard,trash"
+cargo_install "cargo-update" do
+ locked false
+end
+
+cargo_install "broot" do
+ features "clipboard,trash"
+end
+
 cargo_install "du-dust"
-cargo_install "ripgrep"
-cargo_install "fd-find"
-cargo_install "bottom"
+cargo_install "ripgrep" do
+  binname "rg"
+end
+cargo_install "fd-find" do
+  binname "fd"
+end
+cargo_install "bottom" do
+  binname "btm"
+end
 
 # install helix
 git "helix" do
@@ -37,8 +56,8 @@ end
 execute "helix compile" do
   cwd "#{ENV["HOME"]}/src/helix"
   user node[:user]
-  command "cargo install --path helix-term --locked --release"
-  not_if "test -f #{ENV["HOME"]}/bin/helix"
+  command "#{cargo_cmd} install --path helix-term --locked"
+  not_if "which hx"
 end
 
 # add HELIX_RUNTIME to .bashrc
